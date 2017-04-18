76721ceb61c7c8a8291ce959b945418335fd6921
nickpellegrino <npelleg1@nd.edu>
2017-04-18 01:06:59 -0400

    new commit

---
+++ StudyBuddy/ContainerViewController.swift
@@ -0,0 +1,216 @@
+//
+//  ContainerViewController.swift
+//  StudyBuddy
+//
+//  Created by Nick Pellegrino on 3/5/17.
+//  Copyright © 2017 Nick Pellegrino. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+enum SlideOutState {
+    case BothCollapsed
+    case LeftPanelExpanded
+    case RightPanelExpanded
+}
+
+class ContainerViewController: UIViewController {
+    
+    var centerNavigationController: UINavigationController!
+    
+    var viewController: ViewController!
+    var rightViewController: MessageViewController?
+    var leftViewController: SidePanelViewController?
+    
+    let centerPanelExpandedOffset: CGFloat = 15
+    var currentState: SlideOutState = .BothCollapsed {
+        didSet {
+            let shouldShowShadow = currentState != .BothCollapsed
+            showShadowForCenterViewController(shouldShowShadow: shouldShowShadow)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewController = UIStoryboard.viewController()
+        viewController.delegate = self
+        
+        // wrap the centerViewController in a navigation controller, so we can push views to it
+        // and display bar button items in the navigation bar
+        centerNavigationController = UINavigationController(rootViewController: viewController)
+        view.addSubview(centerNavigationController.view)
+        addChildViewController(centerNavigationController)
+        centerNavigationController.didMove(toParentViewController: self)
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ContainerViewController.handlePanGesture))
+        centerNavigationController.view.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+}
+
+extension ContainerViewController: ViewControllerDelegate {
+    
+    func toggleLeftPanel() {
+        let notAlreadyExpanded = (currentState != .LeftPanelExpanded)
+        
+        if notAlreadyExpanded {
+            addLeftPanelViewController()
+        }
+        
+        animateLeftPanel(shouldExpand: notAlreadyExpanded)
+    }
+    
+    func toggleRightPanel() {
+        let notAlreadyExpanded = (currentState != .RightPanelExpanded)
+        
+        if notAlreadyExpanded {
+            addRightPanelViewController()
+        }
+        
+        animateRightPanel(shouldExpand: notAlreadyExpanded)
+    }
+    
+    func collapseSidePanels() {
+        switch (currentState) {
+        case .RightPanelExpanded:
+            toggleRightPanel()
+        case .LeftPanelExpanded:
+            toggleLeftPanel()
+        default:
+            break
+        }
+    }
+    
+    func addLeftPanelViewController() {
+        if (leftViewController == nil) {
+            leftViewController = UIStoryboard.leftViewController()
+            leftViewController!.studyBuddies = viewController.buddies
+            
+            addChildSidePanelController(sidePanelController: leftViewController!)
+        }
+    }
+    
+    func addRightPanelViewController() {
+        if (rightViewController == nil) {
+            rightViewController = UIStoryboard.messageViewController()
+            rightViewController!.messages = viewController.messages
+            
+            addChildMessagePanelController(messagePanelController: rightViewController!)
+        }
+    }
+ 
+    func addChildSidePanelController(sidePanelController: SidePanelViewController) {
+        sidePanelController.delegate = viewController
+        
+        view.insertSubview(sidePanelController.view, at: 0)
+        
+        addChildViewController(sidePanelController)
+        sidePanelController.didMove(toParentViewController: self)
+    }
+ 
+    func addChildMessagePanelController(messagePanelController: MessageViewController){
+        messagePanelController.delegate = viewController
+        
+        view.insertSubview(messagePanelController.view, at: 0)
+        
+        addChildViewController(messagePanelController)
+        messagePanelController.didMove(toParentViewController: self)
+    }
+    
+    func animateRightPanel(shouldExpand: Bool) {
+        if (shouldExpand) {
+            currentState = .RightPanelExpanded
+            
+            animateCenterPanelXPosition(targetPosition: -centerNavigationController.view.frame.width + centerPanelExpandedOffset)
+        } else {
+            animateCenterPanelXPosition(targetPosition: 0) { _ in
+                self.currentState = .BothCollapsed
+                
+                self.rightViewController!.view.removeFromSuperview()
+                self.rightViewController = nil;
+            }
+        }
+    }
+    
+    func animateLeftPanel(shouldExpand: Bool) {
+        if (shouldExpand) {
+            currentState = .LeftPanelExpanded
+            
+            animateCenterPanelXPosition(targetPosition: centerNavigationController.view.frame.width - centerPanelExpandedOffset)
+        } else {
+            animateCenterPanelXPosition(targetPosition: 0) { finished in
+                self.currentState = .BothCollapsed
+                
+                self.leftViewController!.view.removeFromSuperview()
+                self.leftViewController = nil;
+            }
+        }
+    }
+    
+    func animateCenterPanelXPosition(targetPosition: CGFloat, completion: ((Bool) -> Void)! = nil) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+            self.centerNavigationController.view.frame.origin.x = targetPosition
+        }, completion: completion)
+    }
+    
+    func showShadowForCenterViewController(shouldShowShadow: Bool) {
+        if (shouldShowShadow) {
+            centerNavigationController.view.layer.shadowOpacity = 0.8
+        } else {
+            centerNavigationController.view.layer.shadowOpacity = 0.0
+        }
+    }
+}
+
+extension ContainerViewController: UIGestureRecognizerDelegate {
+    // MARK: Gesture recognizer
+    
+    func handlePanGesture(recognizer: UIPanGestureRecognizer) {
+        let gestureIsDraggingFromLeftToRight = (recognizer.velocity(in: view).x > 0)
+        
+        switch(recognizer.state) {
+        case .began:
+            if (currentState == .BothCollapsed) {
+                if (gestureIsDraggingFromLeftToRight) {
+                    addLeftPanelViewController()
+                } else {
+                    addRightPanelViewController()
+                }
+                
+                showShadowForCenterViewController(shouldShowShadow: true)
+            }
+        case .changed:
+            recognizer.view!.center.x = recognizer.view!.center.x + recognizer.translation(in: view).x
+            recognizer.setTranslation(CGPoint.zero, in: view)
+        case .ended:
+            if (leftViewController != nil) {
+                // animate the side panel open or closed based on whether the view has moved more or less than halfway
+                let hasMovedGreaterThanHalfway = recognizer.view!.center.x > view.bounds.size.width
+                animateLeftPanel(shouldExpand: hasMovedGreaterThanHalfway)
+            } else if (rightViewController != nil) {
+                let hasMovedGreaterThanHalfway = recognizer.view!.center.x < 0
+                animateRightPanel(shouldExpand: hasMovedGreaterThanHalfway)
+            }
+        default:
+            break
+        }
+    }
+    
+}
+
+private extension UIStoryboard {
+    class func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: Bundle.main) }
+    
+    class func leftViewController() -> SidePanelViewController? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "LeftViewController") as? SidePanelViewController
+    }
+    
+    class func viewController() -> ViewController? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "ViewController") as? ViewController
+    }
+    
+    class func messageViewController() -> MessageViewController?{
+        return mainStoryboard().instantiateViewController(withIdentifier: "MessageViewController") as? MessageViewController
+    }
+}
---
+++ StudyBuddy/Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  StudyBuddy
+//
+//  Created by Nick Pellegrino on 3/5/17.
+//  Copyright © 2017 Nick Pellegrino. All rights reserved.
+//
+
+import UIKit
+
+class Message {
+    
+    let buddyName: String
+    let message: String
+    
+    init(buddyName: String, message: String) {
+        self.buddyName = buddyName
+        self.message = message
+    }
+    
+}
